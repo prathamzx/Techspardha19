a8aee264e58756ec6f93a9ac6e6d498e8f706c54
pratham-bst <pratham.aggarwal@bluestacks.com>
2020-11-08 14:50:21 +0530

    Updates

---
+++ precache-manifest.e3f90100fd291f630f072e3edb94d6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf2391b60b257152145efabee93dde8",
+    "url": "/Techspardha19/index.html"
+  },
+  {
+    "revision": "ff623863cd408b23c2da",
+    "url": "/Techspardha19/static/css/main.e31ed890.chunk.css"
+  },
+  {
+    "revision": "edfb9b3757aa276d716f",
+    "url": "/Techspardha19/static/js/2.c2b853dc.chunk.js"
+  },
+  {
+    "revision": "ff623863cd408b23c2da",
+    "url": "/Techspardha19/static/js/main.19d01895.chunk.js"
+  },
+  {
+    "revision": "7d416010e808f14c7444",
+    "url": "/Techspardha19/static/js/runtime~main.cad44c08.js"
+  },
+  {
+    "revision": "ce1901dd31099a0d88ad653d509369a7",
+    "url": "/Techspardha19/static/media/grains.ce1901dd.png"
+  }
+]);
